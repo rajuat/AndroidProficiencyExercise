317f32d52a724850d60ce0c811046fc0abeac4b6
Raju At <raju.athokpam@gmail.com>
2020-01-25 13:57:34 +0530

    Integrated with Retrofit and Gson.

--- app/src/main/java/com/example/androidproficiencyexcercise/SwipeRefreshLayoutFragment.java
+++ app/src/main/java/com/example/androidproficiencyexcercise/SwipeRefreshLayoutFragment.java
@@ -8,13 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.androidproficiencyexcercise.backend.APIClient;
+import com.example.androidproficiencyexcercise.backend.APIInterface;
 import com.example.androidproficiencyexcercise.datasource.Stub;
+import com.example.androidproficiencyexcercise.model.AboutCanada;
 import com.example.androidproficiencyexcercise.model.CanadaRecord;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -26,6 +35,8 @@ public class SwipeRefreshLayoutFragment extends Fragment {
     private List<CanadaRecord> recordsOfCanada;
     private SwipeRefreshLayout swipeRefreshLayout;
     private ListView listView;
+
+    private APIInterface apiService;
 
     public SwipeRefreshLayoutFragment() {
         // Required empty public constructor
@@ -41,10 +52,30 @@ public class SwipeRefreshLayoutFragment extends Fragment {
         swipeRefreshLayout = view.findViewById(R.id.swipeToRefresh);
         listView = view.findViewById(R.id.listView);
 
+        apiService = APIClient.getClient().create(APIInterface.class);
+        Call<AboutCanada> nationalFactsCall = apiService.getNationalFacts();
+        nationalFactsCall.enqueue(new Callback<AboutCanada>() {
+            @Override
+            public void onResponse(Call<AboutCanada> call, Response<AboutCanada> response) {
+                AboutCanada body = response.body();
+
+                AppCompatActivity activity = ((AppCompatActivity) getActivity());
+                ActionBar actionBar = activity.getSupportActionBar();
+                if (actionBar != null && body.getTitle() != null) {
+                    actionBar.setTitle(body.getTitle());
+                }
+                NationalFactsAdapter adapter = new NationalFactsAdapter(getContext(), activity, R.layout.row_layout, body.getNationalFacts());
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<AboutCanada> call, Throwable t) {
+                Log.d(TAG, "onFailure: ");
+            }
+        });
         recordsOfCanada = Stub.getRecords();
 
-        RecordAdapter adapter = new RecordAdapter(getContext(), R.layout.row_layout, recordsOfCanada);
-        listView.setAdapter(adapter);
+
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
 
